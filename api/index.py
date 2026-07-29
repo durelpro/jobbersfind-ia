65d46f1d5f9106534f7c6f5f596ee31d03f5887d
durelpro <donfackdurel1980@gmail.com>
2026-07-29 15:24:16 +0200

    Fix rewrite rules for FastAPI routes

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 import sys
 from pathlib import Path
 
-# Ajoute le dossier racine du projet au path Python pour pouvoir importer 'src'
+# Ajoute le dossier racine du projet au path Python pour charger 'src'
 file_dir = Path(__file__).resolve().parent
 root_dir = file_dir.parent
 sys.path.append(str(root_dir))
 
-# Importation de votre application FastAPI principale
+# Import de l'application FastAPI principale
 from src.api.main import app
+
+# Vercel utilise cette variable 'app' comme point d'entrée ASGI
